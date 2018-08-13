d71eeb55ac706cea667f6de85905cb92c3b4c026
FelipeLujan <jfelipelujan@gmail.com>
2018-08-12 22:55:28 -0500

    fine tunning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import Title from "./pagedraw/title";
+import Subtitle from "./pagedraw/subtitle";
+import Element from "./pagedraw/element";
+import Section1 from "./pagedraw/section1";
+import About from "./pagedraw/about";
+import "./app.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Title />
+        <Section1 />
+        <About />
+        <Element
+          tool1={"Bootstrap"}
+          tool2={"Angular5"}
+          tool3={"Firebase"}
+          tool4={"Node.js"}
+          title={"Webconnect"}
+          description={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        />
+        <Element
+          tool1={"Bootstrap"}
+          tool2={"Angular5"}
+          tool3={"Firebase"}
+          tool4={"Node.js"}
+          title={"Webconnect"}
+          description={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        />
+        <Element
+          tool1={"Bootstrap"}
+          tool2={"Angular5"}
+          tool3={"Firebase"}
+          tool4={"Node.js"}
+          title={"Webconnect"}
+          description={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        />
+        <Element
+          tool1={"Bootstrap"}
+          tool2={"Angular5"}
+          tool3={"Firebase"}
+          tool4={"Node.js"}
+          title={"Webconnect"}
+          description={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        />
       </div>
     );
   }
--- src/pagedraw/element.js
+++ src/pagedraw/element.js
@@ -6,41 +6,56 @@ function render() {
   return (
     <div className="element-element-0">
       <div className="element-0">
-        <div className="element-0-0">
-          <div className="element-rectangle-0">
+        <div className="element-rectangle-0">
+          <div className="element-0-0-0">
             <div className="element-0-0-0-0">
               <div className="element-0-0-0-0-0">
-                <div className="element-0-0-0-0-0-0">
-                  <div className="element-implements-0">Implements</div>
-                </div>
-                <div className="element-0-0-0-0-0-1">
-                  <div className="element-tool-1">{this.props.tool1}</div>
-                  <div className="element-tool-2">{this.props.tool2}</div>
-                </div>
-                <div className="element-0-0-0-0-0-2">
-                  <div className="element-0-0-0-0-0-2-0">
-                    <div className="element-0-0-0-0-0-2-0-0">
-                      <div className="element-tool-7">{this.props.tool3}</div>
+                <div className="element-title-0">{this.props.title}</div>
+              </div>
+              <div className="element-0-0-0-0-1">
+                <div className="element-implements-0">Implements</div>
+              </div>
+              <div className="element-0-0-0-0-2">
+                <div className="element-0-0-0-0-2-0">
+                  <div className="element-0-0-0-0-2-0-0">
+                    <div className="element-rectangle_2">
+                      <div className="element-0-0-0-0-2-0-0-0-0">
+                        <div className="element-tool-1">{this.props.tool1}</div>
+                      </div>
+                      <div className="element-0-0-0-0-2-0-0-0-1">
+                        <div className="element-tool-7">{this.props.tool3}</div>
+                      </div>
                     </div>
                   </div>
-                  <div className="element-0-0-0-0-0-2-1">
-                    <div className="element-0-0-0-0-0-2-1-0">
-                      <div className="element-tool-8">{this.props.tool4}</div>
+                </div>
+                <div className="element-0-0-0-0-2-1">
+                  <div className="element-0-0-0-0-2-1-0">
+                    <div className="element-rectangle_3">
+                      <div className="element-0-0-0-0-2-1-0-0-0">
+                        <div className="element-tool-2">{this.props.tool2}</div>
+                      </div>
+                      <div className="element-0-0-0-0-2-1-0-0-1">
+                        <div className="element-tool-8">{this.props.tool4}</div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <img src={this.props.img_src} className="element-image-5" />
             </div>
             <div className="element-0-0-0-1">
-              <div className="element-description-0">
-                {this.props.description}
+              <div className="element-0-0-0-1-0">
+                <img src={this.props.img_src} className="element-image-5" />
               </div>
             </div>
           </div>
-          <div className="element-title-0">{this.props.title}</div>
+          <div className="element-0-0-1">
+            <div className="element-description-0">
+              {this.props.description}
+            </div>
+          </div>
         </div>
       </div>
+      <div className="element-1" />
     </div>
   );
 }
